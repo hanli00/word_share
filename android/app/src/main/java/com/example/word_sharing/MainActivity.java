f0d19483b9df81cbb920ea12066d072a8799a011
hanli00 <hanli00@users.noreply.github.com>
2021-07-20 00:06:10 +0800

    word sharing

---
+++ android/app/src/main/java/com/example/word_sharing/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.word_sharing;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('This is a App Bar'),
+        centerTitle: true,
+        backgroundColor: Colors.purple[600],
+      ),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text('hello!'),
+          TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                primary: Colors.amber,
+                backgroundColor: Colors.black,
+              ),
+              child: Text('button')),
+          Container(
+            color: Colors.cyan,
+            padding: EdgeInsets.all(30.0),
+            child: Text('inside container'),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+        onPressed: () {},
+        backgroundColor: Colors.red[600],
+      ),
+    );
+  }
+}
