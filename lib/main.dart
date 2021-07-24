451c65fa94b99c76c85b7cfdf90025c830619421
hanli00 <hanli00@users.noreply.github.com>
2021-07-24 16:17:18 +0800

    adding Navigator :)

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,94 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: HomePage(),
+  ));
+}
 
-class MyApp extends StatelessWidget {
+class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-          appBar: AppBar(
-            title: Text('Wordsharing'),
-          ),
-          body: Column(
-              children:[
-                Text('function'),
-                ElevatedButton(
-                    onPressed: vocabularylist(),
-                    child: Text('Vocabulary list')
-                ),
-                ElevatedButton(
-                    onPressed: null,
-                    child: Text('Test')
-                ),
-              ]
-          )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Word Sharing'),
+        centerTitle: true,
+      ),
+      body: Column(
+          children:[
+            ElevatedButton(
+            child: Text('Vocabulary List'),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => VocabularyList()),
+              );
+            },
+            ),
+
+            ElevatedButton(
+            child: Text('Quiz'),
+            onPressed: () {
+               Navigator.push(
+                 context,
+               MaterialPageRoute(builder: (context) => Quiz()),
+               );
+            },
+            ),
+        ],
       ),
     );
   }
 }
 
-class vocabularylist extends StatelessWidget {
+class VocabularyList extends StatelessWidget {
   @override
-  Widget build(BuildContext) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-              title: Text('Vocabulary list'),
-            ),
-            body: Column(
-                children:[
-                  ElevatedButton(
-                      onPressed: null,
-                      child: Text('Back')
-                  ),
-                  ElevatedButton(
-                      onPressed: null,
-                      child: Text('+')
-                  ),
-                ]
-            )
-        )
-
-    )
-    )
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Vocabulary List"),
+      ),
+      body: Column(
+        children:[
+          ElevatedButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            child: Text('back'),
+          ),
+          ElevatedButton(
+              onPressed: null,
+              child: Text('+')
+          ),
+        ]
+      ),
+    );
   }
 }
 
+class Quiz extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Quiz"),
+        centerTitle: true,
+      ),
+      body: Column(
+          children:[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text('back'),
+            ),
+            ElevatedButton(
+                onPressed: null,
+                child: Text('+')
+            ),
+          ]
+      ),
+    );
+  }
+}
